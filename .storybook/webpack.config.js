5fdc1b944330d3bee45372e2472be6398d488ddd
Justin Anastos <justin.anastos@gmail.com>
2019-06-27 09:01:20 -0400

    try to quiet storybook build down

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,10 +3,16 @@ module.exports = ({ config }) => {
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('awesome-typescript-loader'),
-      },
-    ],
+        loader: require.resolve("awesome-typescript-loader")
+      }
+    ]
   });
-  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.extensions.push(".ts", ".tsx");
+  config.performance = {
+    ...config.performance,
+    // This isn't a production app so disable size limit warnings
+    maxAssetSize: Infinity,
+    maxEntrypointSize: Infinity
+  };
   return config;
 };
