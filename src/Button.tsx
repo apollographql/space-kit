40be79f18697e746b507c1a6e3825b7e277c8a81
Chang Wang <garnwraly@gmail.com>
2019-07-18 12:38:39 -0400

    split object spreads into separate arrays
    less syntax noise, also prep to convert to css strings

--- src/Button.tsx
+++ src/Button.tsx
@@ -129,7 +129,7 @@ export const Button: React.FC<Props> = ({
     // The `box-shadow` property is copied directly from Zeplin
     boxShadow:
       "inset 0 0 0 1px rgba(18, 21, 26, 0.2), inset 0 -1px 0 0 rgba(18, 21, 26, 0.05), inset 0 2px 2px 0 rgba(18, 21, 26, 0.12)",
-    outline: 0,
+    outline: 0
   };
 
   return (
@@ -139,84 +139,89 @@ export const Button: React.FC<Props> = ({
       {...!disabled && forceActiveState && { "data-force-active-state": true }}
       {...!disabled && forceFocusState && { "data-force-focus-state": true }}
       {...!disabled && forceHoverState && { "data-force-hover-state": true }}
-      css={{
-        "&[disabled]": {
-          backgroundColor: colors.silver.dark,
-          color: colors.grey.light,
-
-          // We need to also set the `:hover` on `:disabled` so it has a higher
-          // specificity than any `:hover` classes passed in. This also means
-          // that both of these need to be overriden if we want to use a custom
-          // disabled color.
-          ":hover": {
+      css={[
+        {
+          "&[disabled]": {
             backgroundColor: colors.silver.dark,
             color: colors.grey.light,
-          },
+
+            // We need to also set the `:hover` on `:disabled` so it has a higher
+            // specificity than any `:hover` classes passed in. This also means
+            // that both of these need to be overriden if we want to use a custom
+            // disabled color.
+            ":hover": {
+              backgroundColor: colors.silver.dark,
+              color: colors.grey.light
+            }
+          }
         },
 
-        ...(variant === "simple" && { backgroundColor: "transparent" }),
+        variant === "simple" && { backgroundColor: "transparent" },
+        {
+          borderRadius: fab ? "100%" : 4,
 
-        borderRadius: fab ? "100%" : 4,
+          borderWidth: 0,
+          ...(variant === "normal" && {
+            boxShadow:
+              "0 1px 4px 0 rgba(18, 21, 26, 0.08), inset 0 0 0 1px rgba(18, 21, 26, 0.2), inset 0 -1px 0 0 rgba(18, 21, 26, 0.05)"
+          }),
 
-        borderWidth: 0,
-        ...(variant === "normal" && {
-          boxShadow:
-            "0 1px 4px 0 rgba(18, 21, 26, 0.08), inset 0 0 0 1px rgba(18, 21, 26, 0.2), inset 0 -1px 0 0 rgba(18, 21, 26, 0.05)",
-        }),
-
-        minWidth: iconOnly
-          ? size === "small"
-            ? 28
+          minWidth: iconOnly
+            ? size === "small"
+              ? 28
+              : size === "large"
+              ? 42
+              : 36
+            : size === "small"
+            ? 76
             : size === "large"
-            ? 42
-            : 36
-          : size === "small"
-          ? 76
-          : size === "large"
-          ? 112
-          : 100,
+            ? 112
+            : 100,
 
-        padding: size === "small" ? `5px 8px` : size === "large" ? 8 : 7,
-        ...(size === "small"
-          ? base.small
-          : size === "large"
-          ? base.large
-          : base.base),
-
-        ...(!disabled && {
+          padding: size === "small" ? `5px 8px` : size === "large" ? 8 : 7
+        },
+        {
+          ...(size === "small"
+            ? base.small
+            : size === "large"
+            ? base.large
+            : base.base)
+        },
+        !disabled && {
           ":hover, &[data-force-hover-state]": {
             backgroundColor: colors.silver.base,
             cursor: "pointer",
             ...(variant === "normal" && {
               // The `box-shadow` property is copied directly from Zeplin
               boxShadow:
-                "0 5px 10px 0 rgba(18, 21, 26, 0.12), inset 0 0 0 1px rgba(18, 21, 26, 0.2), inset 0 -1px 0 0 rgba(18, 21, 26, 0.05)",
-            }),
-          },
-        }),
+                "0 5px 10px 0 rgba(18, 21, 26, 0.12), inset 0 0 0 1px rgba(18, 21, 26, 0.2), inset 0 -1px 0 0 rgba(18, 21, 26, 0.05)"
+            })
+          }
+        },
+        {
+          fontWeight: 600,
 
-        fontWeight: 600,
+          // Disable the outline because we're setting a custom `:active` style
+          outline: 0,
 
-        // Disable the outline because we're setting a custom `:active` style
-        outline: 0,
-
-        ":focus, &[data-force-focus-state]": {
-          // The `box-shadow` property is copied directly from Zeplin
-          boxShadow:
-            "0 1px 4px 0 rgba(18, 21, 26, 0.08), 0 0 0 2px #bbdbff, inset 0 0 0 1px #2075d6, inset 0 -1px 0 0 rgba(18, 21, 26, 0.05)",
+          ":focus, &[data-force-focus-state]": {
+            // The `box-shadow` property is copied directly from Zeplin
+            boxShadow:
+              "0 1px 4px 0 rgba(18, 21, 26, 0.08), 0 0 0 2px #bbdbff, inset 0 0 0 1px #2075d6, inset 0 -1px 0 0 rgba(18, 21, 26, 0.05)"
+          }
         },
 
         // This must come after `:focus` or the `:focus` state will override `:active`
-        ...(!disabled && forceActiveState
+        !disabled && forceActiveState
           ? activeProperties
-          : { ":active": activeProperties }),
-      }}
+          : { ":active": activeProperties }
+      ]}
     >
       <div
         css={{
           alignItems: "center",
           display: "flex",
-          justifyContent: "center",
+          justifyContent: "center"
         }}
       >
         {icon && (
@@ -225,7 +230,7 @@ export const Button: React.FC<Props> = ({
               display: "inline-block",
               height: iconSize,
               margin: iconOnly ? "3px 0" : "0 4px 0",
-              width: iconSize,
+              width: iconSize
             }}
           >
             {icon}
