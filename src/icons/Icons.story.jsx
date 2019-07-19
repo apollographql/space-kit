934dbb3c950b8f090dbae3c54da88e61285193d9
Danielle Man <daniman0808@gmail.com>
2019-07-18 18:29:23 -0700

    use util components for icon story consistency

--- src/icons/Icons.story.jsx
+++ src/icons/Icons.story.jsx
@@ -8,40 +8,10 @@ import { Column } from '../../components-util/Column';
 
 const svgsReq = require.context('./svgs', true, /\.svg$/);
 
-function formatComponentName(basename) {
-  return camelcase(basename.replace(/@\d+x\d+/, '').replace(/-sl$/, ''), {
+const formatName = basename =>
+  camelcase(basename.replace(/@\d+x\d+/, '').replace(/-sl$/, ''), {
     pascalCase: true
   });
-}
-
-function ComponentName({ children }) {
-  return (
-    <div
-      style={{
-        width: 180,
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis'
-      }}
-    >
-      {children}
-    </div>
-  );
-}
-
-function IconRow({ children }) {
-  return (
-    <div
-      style={{
-        marginTop: 16,
-        marginBottom: 16,
-        display: 'flex'
-      }}
-    >
-      {children}
-    </div>
-  );
-}
 
 const colorMap = {};
 Object.keys(colors).forEach(color => {
@@ -51,73 +21,71 @@ Object.keys(colors).forEach(color => {
   });
 });
 
+// Organize all the icons by category. This will create an object with the keys
+// being the categories and the values being an array of {Component, componentName}.
+const groupedIcons = svgsReq.keys().reduce((map, fullname) => {
+  const match = fullname.match(/^\.\/([^\/]+)\/(.+)/);
+  if (!match) {
+    console.warn('Could not match filename', fullname);
+    return map;
+  }
+
+  const [, category, filename] = match;
+  const basename = filename
+    .split('.')
+    .slice(0, -1)
+    .join('.');
+
+  if (!map[category]) map[category] = [];
+  const componentName = formatName(basename);
+
+  map[category].push({
+    basename: componentName,
+    isStreamlineIcon: fullname.includes('-sl'),
+    Component: require(`../../icons/${componentName}.tsx`)[componentName]
+  });
+
+  return map;
+}, {});
+
 storiesOf('Icons', module)
   .addDecorator(withKnobs)
   .add('Catalog', () => {
     const color = select('Color', colorMap, colors.black.base);
-
-    // Organize all the icons by category. This will create an object with the keys
-    // being the categories and the values being an array of {Component, componentName}.
-    const categorizedComponents = svgsReq
-      .keys()
-      .reduce((accumulator, fullFilename) => {
-        const match = fullFilename.match(/^\.\/([^\/]+)\/(.+)/);
-        if (!match) {
-          console.warn('Could not match filename', fullFilename);
-
-          return accumulator;
-        }
-
-        const [, category, filename] = match;
-        const basename = filename
-          .split('.')
-          .slice(0, -1)
-          .join('.');
-
-        if (!accumulator[category]) {
-          accumulator[category] = [];
-        }
-        const componentName = formatComponentName(basename);
-
-        accumulator[category].push({
-          componentName,
-          Component: require(`../../icons/${componentName}.tsx`)[componentName],
-          isStreamlineIcon: fullFilename.includes('-sl')
-        });
-
-        return accumulator;
-      }, {});
-
     return (
       <Page
         title="Icons"
         description="Icons are an essentialy tool in visually communicating concepts to users, while also allowing users to more easily recongize and recall parts of the interface we design."
       >
-        {Object.entries(categorizedComponents).map(
-          ([category, componentArray]) => {
-            return (
-              <Column key={category} title={category}>
-                {componentArray.map(
-                  ({ componentName, Component, isStreamlineIcon }) => (
-                    <IconRow key={componentName}>
-                      <ComponentName>
-                        {componentName}
-                        {isStreamlineIcon ? '-sl' : ''}
-                      </ComponentName>
-                      <Component
-                        style={{
-                          width: 20,
-                          height: 20,
-                          color: color
-                        }}
-                      />
-                    </IconRow>
-                  )
-                )}
-              </Column>
-            );
-          }
-        )}
+        {Object.entries(groupedIcons).map(([category, icons]) => (
+          <Column key={category} title={category}>
+            {icons.map(({ basename, isStreamlineIcon, Component }) => (
+              <div
+                key={basename}
+                style={{ marginTop: 16, marginBottom: 16, display: 'flex' }}
+              >
+                <div style={{ width: 180, textOverflow: 'ellipsis' }}>
+                  {basename}{' '}
+                  {isStreamlineIcon && (
+                    <span
+                      className="font-lbl"
+                      style={{ color: colors.silver.darker }}
+                    >
+                      sl
+                    </span>
+                  )}
+                </div>
+                <Component
+                  style={{
+                    width: 20,
+                    height: 20,
+                    color: color
+                  }}
+                />
+              </div>
+            ))}
+          </Column>
+        ))}
       </Page>
     );
   });
