f6cbeea6d9820a0dc07a34b5d6a93fac902a6225
Justin Anastos <justin.anastos@gmail.com>
2019-07-26 14:24:57 -0400

    Fix modals story

--- src/Modal.story.tsx
+++ src/Modal.story.tsx
@@ -1,9 +1,8 @@
 /** @jsx jsx */
-import { jsx, css } from "@emotion/core";
+import { jsx } from "@emotion/core";
 import { storiesOf } from "@storybook/react";
-import { withKnobs, select } from "@storybook/addon-knobs/react";
 import { Modal } from "./Modal";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import * as colors from "./colors";
 import { Button } from "./Button";
 
@@ -67,99 +66,114 @@ storiesOf("Space Kit/Modal", module)
       />
     </div>
   ))
-  .add("static (small) (✅ primaryAction, ✅ secondaryAction, ✅ bottomLeftText)", () => (
-    <Modal
-      size="small"
-      title="Are you sure you want to remove Jeremy?"
-      primaryAction={
-        <Button
-          color={colors.red.base}
-          css={{ color: colors.white }}
-          type="button"
-        >
-          Yes, remove
-        </Button>
-      }
-      secondaryAction={
-        <Button feel="secondary" type="button">
-          Cancel
-        </Button>
-      }
-      bottomLeftText={
-        <span css={{ color: colors.blue.base }}>More info...</span>
-      }
-    >
-      Jeremy will no longer have access to the MGD-Private. You can always add
-      them back to the organization later.
-    </Modal>
-  ))
-  .add("static (small) (✅ primaryAction, ❌ secondaryAction, ✅ bottomLeftText)", () => (
-    <Modal
-      size="small"
-      title="Are you sure you want to remove Jeremy?"
-      primaryAction={
-        <Button
-          color={colors.red.base}
-          css={{ color: colors.white }}
-          type="button"
-        >
-          Yes, remove
-        </Button>
-      }
-      bottomLeftText={
-        <span css={{ color: colors.blue.base }}>More info...</span>
-      }
-    >
-      Jeremy will no longer have access to the MGD-Private. You can always add
-      them back to the organization later.
-    </Modal>
-  ))
-  .add("static (small) (✅ primaryAction, ❌ secondaryAction, ❌ bottomLeftText)", () => (
-    <Modal
-      size="small"
-      title="Are you sure you want to remove Jeremy?"
-      primaryAction={
-        <Button
-          color={colors.red.base}
-          css={{ color: colors.white }}
-          type="button"
-        >
-          Yes, remove
-        </Button>
-      }
-    >
-      Jeremy will no longer have access to the MGD-Private. You can always add
-      them back to the organization later.
-    </Modal>
-  ))
-  .add("static (medium) (✅ primaryAction, ✅ secondaryAction, ✅ bottomLeftText)", () => (
-    <Modal
-      size="medium"
-      title="Modal Title"
-      primaryAction={
-        <Button color={colors.green.base} css={{ color: colors.white }}>
-          Buy 1 Seat
-        </Button>
-      }
-      secondaryAction={<Button feel="secondary">Cancel</Button>}
-      bottomLeftText={
-        <span css={{ color: colors.blue.base }}>
-          Update Billing Information
-        </span>
-      }
-    >
-      Additional seats needed
-    </Modal>
-  ))
-  .add("static (large) (❌ primaryAction, ❌ secondaryAction, ❌ bottomLeftText) ", () => (
-    <Modal
-      size="large"
-      title="Modal Title"
-      description="Description of this modal or call to action"
-      primaryAction={<Button>Done</Button>}
-    >
-      <div css={{ minHeight: 300, backgroundColor: colors.silver.light }}>
-        content
-      </div>
-    </Modal>
-  ));
+  .add(
+    "static (small) (✅ primaryAction, ✅ secondaryAction, ✅ bottomLeftText)",
+    () => (
+      <Modal
+        size="small"
+        title="Are you sure you want to remove Jeremy?"
+        primaryAction={
+          <Button
+            color={colors.red.base}
+            css={{ color: colors.white }}
+            type="button"
+          >
+            Yes, remove
+          </Button>
+        }
+        secondaryAction={
+          <Button feel="secondary" type="button">
+            Cancel
+          </Button>
+        }
+        bottomLeftText={
+          <span css={{ color: colors.blue.base }}>More info...</span>
+        }
+      >
+        Jeremy will no longer have access to the MGD-Private. You can always add
+        them back to the organization later.
+      </Modal>
+    )
+  )
+  .add(
+    "static (small) (✅ primaryAction, ❌ secondaryAction, ✅ bottomLeftText)",
+    () => (
+      <Modal
+        size="small"
+        title="Are you sure you want to remove Jeremy?"
+        primaryAction={
+          <Button
+            color={colors.red.base}
+            css={{ color: colors.white }}
+            type="button"
+          >
+            Yes, remove
+          </Button>
+        }
+        bottomLeftText={
+          <span css={{ color: colors.blue.base }}>More info...</span>
+        }
+      >
+        Jeremy will no longer have access to the MGD-Private. You can always add
+        them back to the organization later.
+      </Modal>
+    )
+  )
+  .add(
+    "static (small) (✅ primaryAction, ❌ secondaryAction, ❌ bottomLeftText)",
+    () => (
+      <Modal
+        size="small"
+        title="Are you sure you want to remove Jeremy?"
+        primaryAction={
+          <Button
+            color={colors.red.base}
+            css={{ color: colors.white }}
+            type="button"
+          >
+            Yes, remove
+          </Button>
+        }
+      >
+        Jeremy will no longer have access to the MGD-Private. You can always add
+        them back to the organization later.
+      </Modal>
+    )
+  )
+  .add(
+    "static (medium) (✅ primaryAction, ✅ secondaryAction, ✅ bottomLeftText)",
+    () => (
+      <Modal
+        size="medium"
+        title="Modal Title"
+        primaryAction={
+          <Button color={colors.green.base} css={{ color: colors.white }}>
+            Buy 1 Seat
+          </Button>
+        }
+        secondaryAction={<Button feel="secondary">Cancel</Button>}
+        bottomLeftText={
+          <span css={{ color: colors.blue.base }}>
+            Update Billing Information
+          </span>
+        }
+      >
+        Additional seats needed
+      </Modal>
+    )
+  )
+  .add(
+    "static (large) (❌ primaryAction, ❌ secondaryAction, ❌ bottomLeftText) ",
+    () => (
+      <Modal
+        size="large"
+        title="Modal Title"
+        description="Description of this modal or call to action"
+        primaryAction={<Button>Done</Button>}
+      >
+        <div css={{ minHeight: 300, backgroundColor: colors.silver.light }}>
+          content
+        </div>
+      </Modal>
+    )
+  );
