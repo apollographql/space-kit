1635ae1db6e399a445dd90fa82827eb754c80bd7
Justin Anastos <justin.anastos@gmail.com>
2019-07-30 17:48:31 -0400

    working on it

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,8 +7,7 @@ module.exports = ({ config }) => {
         options: {
           compilerOptions: {
             declaration: false,
-            // module: "es2015",
-            target: "esnext",
+            // target: "esnext",
           },
         },
       },
