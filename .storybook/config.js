4e7e986ca1d06b3da8c9834d4cd50c79dd702070
Justin Anastos <justin.anastos@gmail.com>
2019-08-26 10:07:38 -0400

    update storybook config to be modern

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,20 +1,16 @@
+import "../reset.css";
+import "../font.css";
+
 import { configure } from "@storybook/react";
 
-function importAll(r) {
-  r.keys().forEach(r);
-}
-
-function loadStories() {
-  importAll(
-    require.context("../src", true, /\.(?:stories|story)\.(jsx?|tsx?)$/)
-  );
-
-  importAll(
-    require.context("../stories", true, /\.(?:stories|story)\.(jsx?|tsx?)$/)
-  );
-
-  require("../reset.css");
-  require("../font.css");
-}
-
-configure(loadStories, module);
+configure(
+  [
+    require.context("../src", true, /\.(?:stories|story)\.(jsx?|tsx?|mdx)$/),
+    require.context(
+      "../stories",
+      true,
+      /\.(?:stories|story)\.(jsx?|tsx?|mdx)$/
+    ),
+  ],
+  module
+);
