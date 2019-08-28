5dc43a0f766e54af9cc5f3eef041ee345508aeb1
Justin Anastos <justin.anastos@gmail.com>
2019-08-28 17:06:01 -0400

    docs(Button): write docs for button

---
+++ src/Button/button.stories/ButtonLayout.tsx
@@ -0,0 +1,34 @@
+import { ClassNames } from "@emotion/core";
+import React from "react";
+import { Button } from "../Button";
+import { colors } from "../../colors";
+
+interface Props {
+  theme: NonNullable<React.ComponentProps<typeof Button>["theme"]>;
+}
+
+export const ButtonLayout: React.FC<Props> = ({ children, theme }) => (
+  <ClassNames>
+    {({ css, cx }) => (
+      <div
+        className={cx(
+          css({
+            backgroundColor:
+              theme === "light" ? colors.white : colors.black.base,
+            border: "solid 1px #dee2e7",
+            borderRadius: 8,
+            display: "inline-flex",
+            flexDirection: "column",
+
+            "> *": {
+              margin: 8,
+              display: "block",
+            },
+          })
+        )}
+      >
+        {children}
+      </div>
+    )}
+  </ClassNames>
+);
