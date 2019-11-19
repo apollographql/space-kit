d541147be26d86781a39e83c967f8fac739ab04f
Jason Zukewich <jgzuke@gmail.com>
2019-11-19 14:14:29 -0800

    Change loader spin duration to 750ms (#140)
    * change spin duration to 750ms
    
    * Move constants into function

--- src/Loaders/LoadingSpinner.tsx
+++ src/Loaders/LoadingSpinner.tsx
@@ -26,56 +26,59 @@ interface Props {
   size?: Size;
 }
 
-const SPIN = keyframes`
-  0% { transform: rotate(0) }
-  100% { transform: rotate(360deg) }
-`;
-
-const SIZE_MAP: Record<Size, number> = {
-  large: 90,
-  medium: 64,
-  small: 48,
-  xsmall: 32,
-  "2xsmall": 16,
-};
-
-const THEME_MAP: Record<
-  Theme,
-  {
-    orbitColor: CSS.ColorProperty;
-    orbitOpacity: CSS.GlobalsNumber;
-    asteroidColor: CSS.ColorProperty;
-  }
-> = {
-  light: {
-    orbitColor: colors.silver.light,
-    orbitOpacity: 1,
-    asteroidColor: colors.blue.base,
-  },
-  dark: {
-    orbitColor: colors.white,
-    orbitOpacity: 0.5,
-    asteroidColor: colors.white,
-  },
-  grayscale: {
-    orbitColor: colors.silver.darker,
-    orbitOpacity: 1,
-    asteroidColor: colors.grey.light,
-  },
-};
-
 export const LoadingSpinner: React.FC<Props> = ({
   theme = "light",
   size = "medium",
   className,
   ...props
 }) => {
+  /* Length of animation */
+  const DURATION = 1000;
+
+  const SPIN = keyframes`
+    0% { transform: rotate(0) }
+    100% { transform: rotate(360deg) }
+  `;
+
+  const SIZE_MAP: Record<Size, number> = {
+    large: 90,
+    medium: 64,
+    small: 48,
+    xsmall: 32,
+    "2xsmall": 16,
+  };
+
+  const THEME_MAP: Record<
+    Theme,
+    {
+      orbitColor: CSS.ColorProperty;
+      orbitOpacity: CSS.GlobalsNumber;
+      asteroidColor: CSS.ColorProperty;
+    }
+  > = {
+    light: {
+      orbitColor: colors.silver.light,
+      orbitOpacity: 1,
+      asteroidColor: colors.blue.base,
+    },
+    dark: {
+      orbitColor: colors.white,
+      orbitOpacity: 0.5,
+      asteroidColor: colors.white,
+    },
+    grayscale: {
+      orbitColor: colors.silver.darker,
+      orbitOpacity: 1,
+      asteroidColor: colors.grey.light,
+    },
+  };
+
   const { orbitColor, orbitOpacity, asteroidColor } = THEME_MAP[theme];
 
   const pixelSize = SIZE_MAP[size];
 
   const mountTime = React.useRef(Date.now());
-  const mountDelay = isChromatic() ? 0 : -(mountTime.current % 540);
+  const mountDelay = isChromatic() ? 0 : -(mountTime.current % DURATION);
 
   return (
     <svg
@@ -99,7 +102,7 @@ export const LoadingSpinner: React.FC<Props> = ({
       <g transform="translate(50 50)">
         <circle
           css={{
-            animation: `${SPIN} 540ms linear infinite`,
+            animation: `${SPIN} ${DURATION}ms linear infinite`,
             willChange: "transform",
             animationDelay: `${mountDelay}ms`,
           }}
