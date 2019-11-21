e490a9beaced9d54faca84c1b6184d768d3f17a4
Justin Anastos <justin.anastos@gmail.com>
2019-11-21 14:15:01 -0500

    👷 disable animations in chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,16 @@
+import isChromatic from "storybook-chromatic/isChromatic";
+import React from "react";
+import { addDecorator, configure } from "@storybook/react";
+import { SpaceKitProvider } from "../src/SpaceKitProvider";
+
 import "../reset.css";
 import "../font.css";
 
-import { configure } from "@storybook/react";
+addDecorator(story => (
+  <SpaceKitProvider disableAnimations={isChromatic()}>
+    {story()}
+  </SpaceKitProvider>
+));
 
 configure(
   [
