ce76202db7e21f289b93be9a82dbe2f53ee40d92
Justin Anastos <justin.anastos@gmail.com>
2019-12-09 15:58:08 -0500

    Update jest config to handle css files

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     "\\.(stories|story)(\\.|\\/)",
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
       "<rootDir>/__mocks__/fileMock.js",
   },
   preset: "ts-jest",
