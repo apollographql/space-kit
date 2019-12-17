789c73e463c513bba0bdf0920f1b6a6466f1357c
Justin Anastos <justin.anastos@gmail.com>
2019-12-17 16:09:34 -0500

    feat(LoadingSpinner): Add ability to passthrough refs

--- src/Loaders/LoadingSpinner.spec.tsx
+++ src/Loaders/LoadingSpinner.spec.tsx
@@ -1,3 +1,4 @@
+import * as faker from "faker";
 import React from "react";
 import { cleanup, render } from "@testing-library/react";
 import { LoadingSpinner } from "./LoadingSpinner";
@@ -8,4 +9,15 @@ test("it should have `role='progress'`", () => {
   const { getByRole } = render(<LoadingSpinner />);
 
   getByRole("progressbar");
+});
+
+test("it should passthrough ref", () => {
+  const testId = faker.lorem.word();
+  const ref = React.createRef<SVGSVGElement>();
+
+  const { getByTestId } = render(
+    <LoadingSpinner data-testid={testId} ref={ref} />
+  );
+
+  expect(getByTestId(testId)).toEqual(ref.current);
 });
