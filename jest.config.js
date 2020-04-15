5229d14bd2a1f1d7117649641146bd82cd200b8d
Justin Anastos <justin.anastos@gmail.com>
2020-04-15 14:14:19 -0400

    ⚙️ add babel support to jest
    This is required to import .esm files from `node_modules`.

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
       "<rootDir>/__mocks__/fileMock.js",
   },
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-babel",
   setupFilesAfterEnv: ["<rootDir>jest/setup.js"],
   snapshotSerializers: ["jest-emotion"],
   globals: {
@@ -23,4 +23,5 @@ module.exports = {
       },
     },
   },
+  transformIgnorePatterns: ["/node_modules/(?!@popperjs/core).+\\.js$"],
 };
