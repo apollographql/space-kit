e65ff8fcda27473e957d531563aace4a2851bfe6
Justin Anastos <justin@apollographql.com>
2020-11-30 11:50:54 -0500

    🚧 add debugging to Tooltip/Confirmation stories (#207)

--- src/ConfirmationTooltip/ConfirmationTooltip.story.tsx
+++ src/ConfirmationTooltip/ConfirmationTooltip.story.tsx
@@ -5,23 +5,26 @@ import { ConfirmationTooltip } from "../ConfirmationTooltip";
 import { findByText } from "@testing-library/dom";
 import { PerformUserInteraction } from "../shared/PerformUserInteraction";
 import { storiesOf } from "@storybook/react";
+import { DebugTooltip } from "../shared/DebugTooltip";
 
 storiesOf("ConfirmationTooltip", module)
   .add(
     "forced visible",
     () => {
       return (
-        <PerformUserInteraction
-          callback={async () => {
-            userEvent.click(await findByText(document.body, "click"));
-          }}
-        >
-          <div style={{ padding: 50 }}>
-            <ConfirmationTooltip content="default content">
-              <Button>click</Button>
-            </ConfirmationTooltip>
-          </div>
-        </PerformUserInteraction>
+        <DebugTooltip>
+          <PerformUserInteraction
+            callback={async () => {
+              userEvent.click(await findByText(document.body, "click"));
+            }}
+          >
+            <div style={{ padding: 50 }}>
+              <ConfirmationTooltip content="default content">
+                <Button>click</Button>
+              </ConfirmationTooltip>
+            </div>
+          </PerformUserInteraction>
+        </DebugTooltip>
       );
     },
     {
