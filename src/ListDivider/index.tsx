f3104dbf72d57cecd3c8787541510b532ff90389
Justin Anastos <justin@apollographql.com>
2021-02-11 12:49:40 -0500

    AR-2064 Resolve issue where `ListDivider` overflows container (#312)
    This causes the list to scroll side-to-side. Resolved by absolutely
    positioning the `hr` so that it doesn't have any effect on scroll behavior.

--- src/ListDivider/index.tsx
+++ src/ListDivider/index.tsx
@@ -1,13 +1,18 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 /** @jsx jsx */
 import React from "react";
 import { css, jsx } from "@emotion/core";
 import { colors } from "../colors";
+import { ListItem } from "../ListItem";
+import { useListConfig, ListConfigProvider } from "../ListConfig";
+import { listPadding } from "../List";
+import { assertUnreachable } from "../shared/assertUnreachable";
 
 /**
  * Divider between sections in a list
  */
 export const ListDivider: React.FC = (props) => {
+  const listConfig = useListConfig();
+
   // Stop click events so we don't try to close the list when clicking something
   // non-interactive
   const handleClick = React.useCallback<React.MouseEventHandler<HTMLHRElement>>(
@@ -18,22 +23,49 @@ export const ListDivider: React.FC = (props) => {
     [],
   );
 
+  /**
+   * Indicates how much negative space we need to take to make sure the divider
+   * goes edge-to-edge
+   */
+  const horizontalOffset =
+    listConfig.margin === "auto"
+      ? -listPadding
+      : listConfig.margin === "none"
+      ? 0
+      : assertUnreachable(listConfig.margin);
+
   return (
-    <hr
-      {...props}
-      onClick={handleClick}
-      css={css({
-        marginLeft: -8,
-        marginRight: -8,
-        marginTop: 8,
-        marginBottom: 8,
-        borderTopColor: colors.silver.base,
-        borderTopWidth: 1,
-        borderTopStyle: "solid",
-        borderBottomWidth: 0,
-        borderLeftWidth: 0,
-        borderRightWidth: 0,
-      })}
-    />
+    <ListConfigProvider truncate={false}>
+      <ListItem
+        interactive={false}
+        css={css({
+          alignItems: "stretch",
+          justifyItems: "center",
+          paddingLeft: 0,
+          paddingRight: 0,
+          position: "relative",
+          height: 17,
+        })}
+      >
+        <hr
+          {...props}
+          onClick={handleClick}
+          css={{
+            borderBottomWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopColor: colors.silver.base,
+            borderTopStyle: "solid",
+            borderTopWidth: 1,
+            bottom: 0,
+            left: horizontalOffset,
+            margin: 0,
+            position: "absolute",
+            right: horizontalOffset,
+            top: 8,
+          }}
+        />
+      </ListItem>
+    </ListConfigProvider>
   );
 };
